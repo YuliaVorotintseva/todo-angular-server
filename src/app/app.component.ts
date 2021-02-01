b5e5e501e13219cbc84d8897d6ed53820592e741
Yulia Vorotintseva <vip.vy00@mail.ru>
2021-02-01 10:53:19 +0600

    Added post, fetch and delete methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import {Component, OnInit} from '@angular/core'
+import {delay} from 'rxjs/operators'
+
+export interface Todo {
+  title: string,
+  completed: boolean,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,6 +14,40 @@ import {Component, OnInit} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  ngOnInit() {}
-}
+  todos: Todo[] = []
+  title: string = ''
+  loading: boolean = false
 
+  constructor(private client: HttpClient){}
+
+  ngOnInit() {
+    this.fetchTodo()
+  }
+
+  addTodo() {
+    if(!this.title) return
+
+    const todo: Todo = {
+      title: this.title,
+      completed: false
+    }
+    
+    this.client.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+      .subscribe(todo => this.todos.push(todo))
+  }
+
+  fetchTodo() {
+    this.loading = true
+    this.client.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .pipe(delay(2000))
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
+      })
+  }
+
+  removeTodo(id: number = 0) {
+    this.client.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .subscribe(() => this.todos = this.todos.filter(todo => todo.id !== id))
+  }
+}
